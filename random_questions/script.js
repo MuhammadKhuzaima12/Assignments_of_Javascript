a04412d9e093c53bd74c0820cab9e3bd006ccc76
Muhammad Khuzaima <khuzaimasujaila@gmail.com>
2025-06-19 00:56:08 +0500

    random qusestion

--- random_questions/script.js
+++ random_questions/script.js
@@ -1,11 +1,26 @@
-var inputs = [];
-for (var i = 1; i <= 10; i++) {
-    inputs[ i - 1] = prompt("Enter a number");
-};
-for (var j = 0; j < 10; j++){
-    if (inputs[j] % 2 == 0) {
-        document.write(`${inputs[j]} is even.<br>`);
-    } else {
-        document.write(`${inputs[j]} is odd.<br>`);
-    }
-};
+// Q: We need to take 10 input prompt from user and need to identify the even ad odd number and 
+// also  how can we we make multiple prompt
+
+// var inputs = [];
+// for (var i = 1; i <= 10; i++) {
+//     inputs[ i - 1] = prompt("Enter a number");
+// };
+// for (var j = 0; j < 10; j++){
+//     if (inputs[j] % 2 == 0) {
+//         document.write(`${inputs[j]} is even.<br>`);
+//     } else {
+//         document.write(`${inputs[j]} is odd.<br>`);
+//     }
+// };
+
+
+// Q:
+
+// var str = "Apple";
+// var str_arr = [];
+// for (var i = 0; i < str.length; i++) {
+//     str_arr[i] = str.charCodeAt(i);
+// };
+// str_arr[0].c
+// str_codes = str_arr.slice(0, 5);
+// console.log(str_codes);
